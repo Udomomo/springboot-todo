0278713cee42bfc0512e26148cd7d8febb1a2cc9
Udomomo <udomomo@gmail.com>
2020-03-01 02:17:40 +0900

    create Error and Success type and add them to type mapping

---
+++ src/main/java/com/udomomo/springboottodo/Success.java
@@ -0,0 +1,11 @@
+package com.udomomo.springboottodo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Success {
+    private Integer code;
+    private String message;
+}
